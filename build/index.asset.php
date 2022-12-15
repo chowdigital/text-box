b7387d873c04d0852d186bdea1fb16a0da858c42
Sean Cloudsdale <sean@cloudsdale.co.uk>
2022-12-15 12:42:52 +0000

    lesson icon

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f5e6b814e1de75768b5b381e22df3b7f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '5b57ff8f737e4d71f64a2343e9e3a519');
